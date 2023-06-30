e199160c22f096ac135870057e7558e763c693d6
Graham McIntire <graham@mcintire.me>
2023-06-30 09:01:44 -0500

    bump elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Routeros.Mixfile do
     [
       app: :routeros,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
